62e1fd37ffe69753fee00f2c2182e4e94886b543
Mark Anderson <YaydIh.github.com@ma7.org>
2015-05-18 22:54:02 -0500

    preliminary draft

--- server.R
+++ server.R
@@ -1,6 +1,11 @@
 library(shiny)
+library(lubridate)
 
 myData <- read.csv(file="data/9898.csv", header=TRUE, sep=",")
+myData$ldate <- mdy_hms(myData$TimeCreate)
+myData$hour <- hour(myData$ldate)
+myData$wday <- wday(myData$ldate)
+
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
@@ -13,13 +18,64 @@ shinyServer(function(input, output) {
   #  2) Its output type is a plot
 
 
-  output$myText <- renderText({ paste(nrow(myData)) })
-  output$distPlot <- renderPlot({
-    x    <- faithful[, 2]  # Old Faithful Geyser data
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  output$myText1 <- renderText({ paste(input$checkGroup1) })
+  output$myText2 <- renderText({ paste(input$checkGroup2) })
 
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  dataInput1 <- reactive({
+    myData[myData$wday %in% input$checkGroup1,c("hour")]
   })
+  dataInput2 <- reactive({
+    myData[myData$wday %in% input$checkGroup2,c("hour")]
+  })
+  output$distPlot1 <- renderPlot({
+    x    <- dataInput1()
+    bins <- -1:23
+    # draw the histogram with the specified number of bins
+    hist(
+     x, 
+     breaks = bins, 
+     main="1", 
+     col = 'lightpink', 
+     border = 'white',
+     freq=FALSE,
+     ylim=c(0,.1))
+    lines(density(dataInput1(), adjust = 2), col = "red")
+    lines(density(dataInput2(), adjust = 2), col = "green")
+    lines(density(myData$hour, adjust = 2), col = "blue")
+  })
+  output$distPlot2 <- renderPlot({
+    x    <- dataInput2()
+    bins <- -1:23
+    # draw the histogram with the specified number of bins
+    hist(
+     x, 
+     breaks = bins, 
+     main="1", 
+     col = 'lightgreen', 
+     border = 'white',
+     freq=FALSE,
+     ylim=c(0,0.1))
+    lines(density(dataInput1(), adjust = 2), col = "red")
+    lines(density(dataInput2(), adjust = 2), col = "green")
+    lines(density(myData$hour, adjust = 2), col = "blue")
+  })
+  output$distPlot3 <- renderPlot({
+    x    <- myData$hour  
+    bins <- -1:23
+    # draw the histogram with the specified number of bins
+    hist(
+     x, 
+     breaks = bins, 
+     main="1", 
+     col = 'lightblue', 
+     border = 'white',
+     freq=FALSE,
+     ylim=c(0,0.1))
+    lines(density(dataInput1(), adjust = 2), col = "red")
+    lines(density(dataInput2(), adjust = 2), col = "green")
+    lines(density(myData$hour, adjust = 2), col = "blue")
+     
+  })
+  
 })
 
--- ui.R
+++ ui.R
@@ -9,17 +9,42 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for the number of bins
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+     checkboxGroupInput(
+       "checkGroup1",
+       label = h4("Graph 1"),
+       choices = list( 
+         'Sunday'    = 1,
+         'Monday'    = 2,
+         'Tuesday'   = 3,
+         'Wednesday' = 4,
+         'Thursday'  = 5,
+         'Friday'    = 6,
+         'Saturday'  = 7),
+       selected= 1:7
+     ),
+     hr(),
+     checkboxGroupInput(
+       "checkGroup2",
+       label = h4("Graph 2"),
+       choices = list( 
+         'Sunday'    = 1,
+         'Monday'    = 2,
+         'Tuesday'   = 3,
+         'Wednesday' = 4,
+         'Thursday'  = 5,
+         'Friday'    = 6,
+         'Saturday'  = 7),
+       selected= 1:7
+     )
     ),
 
     # Show a plot of the generated distribution
     mainPanel(
-      textOutput("myText"),
-      plotOutput("distPlot")
+      textOutput("myText1"),
+      textOutput("myText2"),
+      plotOutput("distPlot1"),
+      plotOutput("distPlot2"),
+      plotOutput("distPlot3")
     )
   )
 ))
